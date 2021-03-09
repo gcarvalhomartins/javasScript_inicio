0cc13ee105f3d1d025caf3d5fbcef25bbe42cdac
Gabriel Martins <gcarvalhomartins23@gmail.com>
2021-03-09 16:36:29 -0400

    atualizando

--- função/testando.js
+++ função/testando.js
@@ -27,6 +27,6 @@ function NotBrowser(Criarusuario){
             newusuario:()=>console.log(`olá novo ${Criarusuario},seja bem vindo a plataforma `)
          }
     }
-const p9=NotBrowser('João marcelo ')
-p9.newusuario()
+const p9=NotBrowser('')
+
 

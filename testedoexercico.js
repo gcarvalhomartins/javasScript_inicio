8b4cf6b049e5b0bc3d4e826535ca5ad9c4287bad
Gabriel Martins <gcarvalhomartins23@gmail.com>
2021-03-20 13:39:10 -0400

    upadte tarefas

--- testedoexercico.js
+++ testedoexercico.js
@@ -54,3 +54,25 @@ expondousuario.Newusuario()
 // testando saida de seja be vindo ao úsuario 
 
 
+// testando criação de análise de OO 
+function produto(nome,preco,desconto){
+    const self=this
+    self.nome=nome
+    self.desconto=()=>{
+        return preco*(1-desconto)
+    }
+}
+const p1= new produto('Notebook',1499.99,0.4)
+console.log(p1.desconto())
+
+function Analise(nome,SB,ausencia){
+  return {nome,
+    SB,
+    ausencia,
+    Calculodesalario(){
+        return (SB/30)*(30-ausencia)
+    }
+  }
+}
+const funcionario1=new Analise('Matheus',5000.00,20)
+console.log(funcionario1.Calculodesalario())
